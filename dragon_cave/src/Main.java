db039f021abf4e320fedf451e00ad1b903a11392
Irma Mejia <ijanettmejia@gmail.com>
2022-07-17 14:11:44 -0400

    Add game functionality

--- dragon_cave/src/Main.java
+++ dragon_cave/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String outMsg = "You are in a land full of dragons. In front of you,\nyou see two caves. In one cave, the dragon is friendly\nand will share his treasure with you. The other dragon\nis greedy and hungry and will eat you on sight.\nWhich cave will you go into? (1 or 2)";
+        System.out.println(outMsg);
+        Scanner userInput = new Scanner(System.in);
+        String input = userInput.nextLine();
+
+        String result;
+        switch (input) {
+            case "1":
+                result = "You approach the cave...\nIt's dark and spooky...\nA large dragon jumps out in front of you! He opens his jaws and...\nGobbles you down in one bite!";
+                break;
+            case "2":
+                result = "Great choice! You're still alive and a bit richer!";
+                break;
+            default:
+                result = "Invalid option. Exiting game...";
+        };
+        System.out.println(result);
     }
 }
